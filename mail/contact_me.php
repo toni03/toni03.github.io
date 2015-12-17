32a34334a7754f2d3d6ed6fcf79d6c8986877c4e
Toni <tony.carreiro.03@gmail.com>
2015-12-17 20:18:40 +0100

    Modificaciones para verificar el envío de mails versión 3

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -21,6 +21,10 @@ $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@xestasystems.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
+if (mail($to,$email_subject,$email_body,$headers)) {
+	echo "Email sended correctly";
+} else {
+	echo "Email fail while we was sending";
+}
 return true;			
 ?>
